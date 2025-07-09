24d1235635f579c6823431d87cc541d1bd46351c
n1clude <56724583+n1clud3@users.noreply.github.com>
2025-07-09 22:59:27 +0200

    Specifically create fonts for Linux

--- lua/hud/gogm_hud_fonts.lua
+++ lua/hud/gogm_hud_fonts.lua
@@ -1,23 +1,57 @@
+if (system.IsLinux()) then
+    surface.CreateFont("GOGmScoreboardTitle", {
+        font = "inter_variable.ttf",
+        extended = true,
+        weight = 900,
+        size = 32
+    })
+
+    surface.CreateFont("GOGmScoreboardPlayerName", {
+        font = "inter_variable.ttf",
+        extended = true,
+        weight = 600,
+        size = 22
+    })
+
+    surface.CreateFont("GOGmScoreboardRegular", {
+        font = "inter_variable.ttf",
+        extended = true,
+        weight = 400,
+        size = 24
+    })
+
+    surface.CreateFont("GOGmScoreboardNumbers", {
+        font = "fira_mono_bold.ttf",
+        weight = 600,
+        size = 20
+    })
+
+    return
+end
+
 surface.CreateFont("GOGmScoreboardTitle", {
-  font = "Inter Variable Text Black",
-  extended = true,
-  size = 32
+    font = "Inter Variable",
+    extended = true,
+    weight = 900,
+    size = 32
 })
 
 surface.CreateFont("GOGmScoreboardPlayerName", {
-  font = "Inter Variable Text SemiBold",
-  extended = true,
-  size = 22
+    font = "Inter Variable",
+    extended = true,
+    weight = 600,
+    size = 22
 })
 
 surface.CreateFont("GOGmScoreboardRegular", {
-  font = "Inter Variable Text",
-  extended = true,
-  size = 24
+    font = "Inter Variable",
+    extended = true,
+    weight = 400,
+    size = 24
 })
 
 surface.CreateFont("GOGmScoreboardNumbers", {
-  font = "Fira Mono",
-  weight = 600,
-  size = 20
+    font = "Fira Mono",
+    weight = 600,
+    size = 20
 })
